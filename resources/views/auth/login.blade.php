9c5a975308c3d8ca2c113ace6730084fd147cbb6
Soban-Majeed <sobanraja14@gmail.com>
2024-10-07 18:52:21 +0500

    add animation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,122 +1,153 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="relative h-screen w-screen">
+    <div class="relative w-screen h-screen">
         <div>
-            <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  z-20">
-                <img class="w-[500px] object-contain" style="transform: rotateY(180deg)"
+            <div class="absolute z-20 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <img id="henImage" class="w-[500px] object-contain" style="transform: rotateY(180deg)"
                     src="{{ asset('assets/hen-avatar-withbg.png') }}" alt="hen">
             </div>
-            <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  z-10">
-                <div style="box-shadow: 0px 0px 179px 300px #FCB276A0;">
-
-                </div>
+            <div class="absolute z-10 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <div style="box-shadow: 0px 0px 179px 300px #FCB276A0;"></div>
             </div>
-
         </div>
 
-        <div class="flex justify-between h-[80vh] pt-[10vh] mx-[50px] lg:mx-[100px] xl:mx-[180px] z-50 relative ">
-            {{-- <div class=" max-w-[600px] px-8 flex flex-col justify-center items-center h-full w-full rounded-2xl "
+        <div id="mainContent" class="flex justify-between h-[80vh] pt-[10vh] mx-[50px] lg:mx-[100px] xl:mx-[180px] z-50 relative transition-all duration-700">
+            <!-- Welcome Section -->
+            <div id="welcomeDiv" class="flex flex-col justify-center items-center h-full w-full relative z-50 max-w-[600px] transition-all duration-700 ease-in-out">
+                <div>
+                    <h2 class="text-[60px] text-customOrangeDark font-bold leading-none">
+                        <span class="text-[50px]">Welcome to</span><br> Poultry Bazar
+                    </h2>
+                    <p class="flex justify-start mt-4 font-normal text-black text-custom16">
+                        Start your new journey with us and join <br> our community
+                    </p>
+                </div>
+                <button id="switchToLoginBtn" class="gradient-bg w-[300px] text-white rounded-full text-lg font-semibold h-14 mt-40">
+                    Get Access
+                </button>
+            </div>
+
+            <!-- Login Form Section -->
+            <div id="loginDiv" class="max-w-[600px] px-8 flex flex-col justify-center items-center h-full w-full rounded-2xl transition-all duration-700 ease-in-out"
                 style="box-shadow: 0px 0px 8px 0px #00000026; background:rgba(255, 255, 255, 0.389)">
                 <div class="w-full">
                     <div>
                         <h1 class="text-customBlackColor font-bold text-[44px] text-center">Log in</h1>
                     </div>
-                    <div class="mt-20 ">
-                        <div>
-                            <label for="" class="block text-customGrayColorDark text-sm">Email</label>
-                            <input type="text"
-                                class="border border-gray-400 bg-white rounded-2xl mt-1  placeholder:text-customGrayColorDark w-full placeholder:text-sm "
-                                placeholder="Enter your email">
-                        </div>
-                        <div class="mt-5">
-                            <label for="" class="block text-customGrayColorDark text-sm ">Password</label>
-                            <input type="text"
-                                class="border border-customGrayColorDark bg-white rounded-2xl mt-1  placeholder:text-customGrayColorDark w-full placeholder:text-sm "
-                                placeholder="Enter your password">
-                        </div>
-                        <div>
-                            <p class="text-customOrangeDark text-xs text-right ">Forgot password?</p>
-                        </div>
-                        <button class="gradient-bg w-full text-white rounded-full text-lg font-semi-bold mt-8 h-14">Log
-                            in</button>
-                    </div>
-                </div>
-            </div> --}}
-            {{-- <div class="flex flex-col justify-center items-center h-full w-full relative z-50  max-w-[600px]">
-                <div>
-                    <h2 class="text-[60px] text-customOrangeDark  font-bold leading-none "><span class="text-[50px]">Welcome
-                            to</span> <br> Poultry Bazar</h2>
-                    <p class="text-black text-custom16 font-normal mt-4 flex justify-start ">Start your new journey with
-                        us and
-                        join <br> our
-                        community
-                    </p>
-                </div>
-                <button class="gradient-bg w-[300px] text-white rounded-full text-lg font-semibold  h-14 mt-40">Get
-                    Access</button>
-            </div> --}}
-
-
-            <div class="flex flex-col justify-center items-center h-full w-full relative z-50  max-w-[600px]">
-                <div>
-                    <h2 class="text-[60px] text-customOrangeDark  font-bold leading-none "><span class="text-[50px]">Welcome
-                            to</span> <br> Poultry Bazar</h2>
-                    <p class="text-black text-custom16 font-normal mt-4 flex justify-start ">Start your new journey with
-                        us and
-                        join <br> our
-                        community
-                    </p>
-                </div>
-                <button
-                    class="gradient-bg w-[300px] text-white rounded-full text-lg font-semibold  h-14 mt-40">Login</button>
-            </div>
-
-            <div class=" max-w-[600px] px-8 flex flex-col justify-center items-center h-full w-full rounded-2xl "
-                style="box-shadow: 0px 0px 8px 0px #00000026;  background:rgba(255, 255, 255, 0.389)">
-                <div class="w-full">
-                    <div>
-                        <h1 class="text-customBlackColor font-bold text-[44px] text-center">Get Access</h1>
-                    </div>
                     <form action="">
-                        <div class="mt-20 ">
+                        <div class="mt-20">
                             <div>
-                                <label for="" class="block text-customGrayColorDark text-sm">Email</label>
-                                <input type="text"
-                                    class="border border-gray-400 bg-white rounded-2xl mt-1  placeholder:text-customGrayColorDark w-full placeholder:text-sm "
-                                    placeholder="Enter your email">
-                            </div>
-                            <div>
-                                <label for="" class="block text-customGrayColorDark text-sm mt-5">Email</label>
-                                <input type="text"
-                                    class="border border-gray-400 bg-white rounded-2xl mt-1  placeholder:text-customGrayColorDark w-full placeholder:text-sm "
-                                    placeholder="Enter your email">
-                            </div>
-                            <div>
-                                <label for="" class="block text-customGrayColorDark text-sm mt-5">Email</label>
-                                <input type="text"
-                                    class="border border-gray-400 bg-white rounded-2xl mt-1  placeholder:text-customGrayColorDark w-full placeholder:text-sm "
-                                    placeholder="Enter your email">
+                                <label for="email" class="block text-sm text-customGrayColorDark">Email</label>
+                                <input type="email" id="email" class="w-full mt-1 bg-white border border-gray-400 rounded-2xl placeholder:text-customGrayColorDark placeholder:text-sm" placeholder="Enter your email">
                             </div>
                             <div class="mt-5">
-                                <label for="" class="block text-customGrayColorDark text-sm mt-5 ">Password</label>
-                                <input type="text"
-                                    class="border border-customGrayColorDark bg-white rounded-2xl mt-1  placeholder:text-customGrayColorDark w-full placeholder:text-sm "
-                                    placeholder="Enter your password">
+                                <label for="password" class="block text-sm text-customGrayColorDark">Password</label>
+                                <input type="password" id="password" class="w-full mt-1 bg-white border border-customGrayColorDark rounded-2xl placeholder:text-customGrayColorDark placeholder:text-sm" placeholder="Enter your password">
                             </div>
-
-                            <button
-                                class="gradient-bg w-full text-white rounded-full text-lg font-semi-bold mt-8 h-14">Next</button>
+                            <div>
+                                <p class="text-xs text-right text-customOrangeDark">Forgot password?</p>
+                            </div>
+                            <button type="submit" class="w-full mt-8 text-lg text-white rounded-full gradient-bg font-semi-bold h-14">Log in</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-        <div class="absolute bottom-5 flex justify-center  w-screen">
 
-            <p class="text-black text-sm "> Powered by <span class="text-customOrangeDark">Poul3yBazar</span> &
-                Developed by
+        <div id="signupSection" class="flex justify-between h-[80vh] pt-[10vh] mx-[50px] lg:mx-[100px] xl:mx-[180px] z-50 relative hidden">
+            <!-- Signup Form Section (Initially hidden) -->
+            <div class="max-w-[600px] px-8 flex flex-col justify-center items-center h-full w-full rounded-2xl"
+                style="box-shadow: 0px 0px 8px 0px #00000026; background:rgba(255, 255, 255, 0.389)">
+                <div class="w-full">
+                    <div>
+                        <h1 class="text-customBlackColor font-bold text-[44px] text-center">Get Access</h1>
+                    </div>
+                    <form action="">
+                        <div class="mt-20">
+                            <div>
+                                <label for="" class="block text-sm text-customGrayColorDark">Email</label>
+                                <input type="text"
+                                    class="w-full mt-1 bg-white border border-gray-400 rounded-2xl placeholder:text-customGrayColorDark placeholder:text-sm"
+                                    placeholder="Enter your email">
+                            </div>
+                            <div>
+                                <label for="" class="block mt-5 text-sm text-customGrayColorDark">Confirm Email</label>
+                                <input type="text"
+                                    class="w-full mt-1 bg-white border border-gray-400 rounded-2xl placeholder:text-customGrayColorDark placeholder:text-sm"
+                                    placeholder="Confirm your email">
+                            </div>
+                            <div>
+                                <label for="" class="block mt-5 text-sm text-customGrayColorDark">Email</label>
+                                <input type="text"
+                                    class="w-full mt-1 bg-white border border-gray-400 rounded-2xl placeholder:text-customGrayColorDark placeholder:text-sm"
+                                    placeholder="Enter your email">
+                            </div>
+                            <div class="mt-5">
+                                <label for="" class="block mt-5 text-sm text-customGrayColorDark">Password</label>
+                                <input type="password"
+                                    class="w-full mt-1 bg-white border border-customGrayColorDark rounded-2xl placeholder:text-customGrayColorDark placeholder:text-sm"
+                                    placeholder="Enter your password">
+                            </div>
+
+                            <button class="w-full mt-8 text-lg text-white rounded-full gradient-bg font-semi-bold h-14">Next</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="flex flex-col justify-center items-center h-full w-full relative z-50 max-w-[600px]">
+                <div>
+                    <h2 class="text-[60px] text-customOrangeDark font-bold leading-none">
+                        <span class="text-[50px]">Join Us at</span><br> Poultry Bazar
+                    </h2>
+                    <p class="flex justify-start mt-4 font-normal text-black text-custom16">
+                        Become a member and start your journey with <br> Poultry Bazar
+                    </p>
+                    <button type="submit" class="w-full mt-8 text-lg text-white rounded-full gradient-bg font-semi-bold h-14">Get Access</button>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="absolute flex justify-center w-screen bottom-5">
+            <p class="text-sm text-black">Powered by <span class="text-customOrangeDark">Poul3yBazar</span> & Developed by
                 <a target="_blank" class="text-blue-500" href="https://thewebconcept.com/">TheWebConcept</a>.
             </p>
         </div>
     </div>
+
+    <script>
+        const switchToLoginBtn = document.getElementById('switchToLoginBtn');
+        const mainContent = document.getElementById('mainContent');
+        const signupSection = document.getElementById('signupSection');
+        const henImage = document.getElementById('henImage');
+        const welcomeDiv = document.getElementById('welcomeDiv');
+        const loginDiv = document.getElementById('loginDiv');
+        const switchToLogin = document.getElementById('switchToLogin');
+
+        switchToLoginBtn.addEventListener('click', function () {
+            // Flip the positions of the welcome and login divs with animation
+            welcomeDiv.classList.toggle('order-last');
+            loginDiv.classList.toggle('order-first');
+
+            // Transition between login form and signup form
+            setTimeout(() => {
+                mainContent.classList.add('hidden');
+                signupSection.classList.remove('hidden');
+            }, 700); // Wait for animation to complete before switching
+
+            // Animate the image flip
+            henImage.style.transition = "transform 0.6s ease";
+            henImage.style.transform = "rotateY(0deg)";
+        });
+
+        switchToLogin.addEventListener('click', function () {
+            // Reverse the div positions and show the login form
+            signupSection.classList.add('hidden');
+            mainContent.classList.remove('hidden');
+
+            // Animate the image flip back to original
+            henImage.style.transform = "rotateY(180deg)";
+        });
+    </script>
 @endsection
