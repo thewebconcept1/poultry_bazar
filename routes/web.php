95a26bc7244b7b5a1dc5cf1f593761cdba626785
MuhammadNasir1 <nmian7080@gmail.com>
2024-10-10 11:11:03 +0500

    add layout

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['custom_auth'])->group(function () {
 
     Route::get('/', function () {
-        return view('layouts.layout');
+        return view('dashboard');
     });
-    
 });
 
 Route::get('/login', function () {
