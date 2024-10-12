5cd3a85dbd16111d79be1055d6b2242037b47f24
Soban-Majeed <sobanraja14@gmail.com>
2024-10-12 18:06:04 +0500

    fix table issue

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,13 +1,15 @@
-<table id="datatable" class="dataTable ">
-    <thead class="gradient-bg text-white font-semibold ">
-        <tr class="border-b">
+<table id="datatable" class="dataTable">
+    <thead class="gradient-bg w-full  text-white font-semibold ">
+        <tr class="border-b ">
             @foreach ($headers as $header)
                 <th>{{ $header }}</th>
             @endforeach
         </tr>
     </thead>
-    <tbody class="border-b border-gray-500">
+    <tbody class="border-b  border-gray-500">
+        {{-- <tr class="border-b"> --}}
 
+        </tr>
         {!! $body !!}
 
     </tbody>
--- resources/views/operators.blade.php
+++ resources/views/operators.blade.php
@@ -1,13 +1,29 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="mx-6 my-4 min-h-[88vh] card-shadow rounded-lg">
-        @php
-            $headers = ['ID', 'Name', 'Email'];
-            $body = '<tr><td>1</td><td>John Doe</td><td>john@example.com</td></tr>
-         <tr><td>2</td><td>Jane Doe</td><td>jane@example.com</td></tr>';
+    <div class="w-full pt-10 min-h-[88vh] gradient-border  rounded-lg">
+        <div>
+            <h1 class="text-3xl font-bold ps-5">Operators</h1>
+        </div>
 
+        <a href=""></a>
+        @php
+            $headers = ['Sr.', 'Image', 'Name', 'Email', 'Phone Number', 'Last Active', 'status', 'Action'];
+            $body =
+                "<tr>
+                <td>1</td>
+                <td><img src='" .
+                asset('assets/Profile photo (1) 1.png') .
+                "' alt=''></td>
+                <td>Kurt Weissnat</td>
+                <td>KurtWeissnat@email.com</td>
+                <td>1234564</td>
+                <td>1234564</td>
+                <td>1234564zz</td>
+                <td>1234564zz</td>
+        </tr>";
         @endphp
 
         <x-table :headers="$headers" :body="$body" />
     </div>
+    <img src="" alt="">
 @endsection
