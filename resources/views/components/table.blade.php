d99343d030024a2160040cc77e42cafd6b6ad5df
Soban-Majeed <sobanraja14@gmail.com>
2024-10-21 16:11:34 +0500

    dashboard changes

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,12 +1,12 @@
-<table id="datatable" class="dataTable">
-    <thead class="gradient-bg w-full  text-white font-semibold ">
-        <tr class="border-b ">
+<table id="datatable" class="container dataTable">
+    <thead class="w-full font-semibold text-white gradient-bg ">
+        <tr class="w-full border-b">
             @foreach ($headers as $header)
                 <th>{{ $header }}</th>
             @endforeach
         </tr>
     </thead>
-    <tbody class="border-b  border-gray-500">
+    <tbody class="border-b border-gray-500">
         {{-- <tr class="border-b"> --}}
 
         </tr>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,40 +8,40 @@
                 class="flex items-center justify-between w-full h-full p-4 bg-orange-100 border-4 shadow-sm border-customOrangeLight rounded-xl">
                 <div>
                     <h2 class="mb-1 text-sm font-medium text-gray-400">Operators</h2>
-                    <p class=" text-2xl   font-bold text-gray-800">72</p>
+                    <p class="text-2xl font-bold text-gray-800 ">72</p>
                 </div>
-                <div class="flex justify-center items-center w-14 h-14  xl:p-5 rounded-full bg-customOrangeDark">
-                    <img class="min-w-7" src="{{ asset('assets/icons/dashborad icons1.png') }}" alt="">
+                <div class="flex items-center justify-center rounded-full">
+                    <img class="h-14 w-14" src="{{ asset('assets/icons/dex icon1.png') }}" alt="">
                 </div>
             </div>
             <div
                 class="flex items-center justify-between w-full h-full p-4 bg-orange-100 border-4 shadow-sm border-customOrangeLight rounded-xl">
                 <div>
                     <h2 class="mb-1 text-sm font-medium text-gray-400">Cities</h2>
-                    <p class=" text-2xl font-bold text-gray-800">5,732</p>
+                    <p class="text-2xl font-bold text-gray-800 ">5,732</p>
                 </div>
-                <div class="w-14 h-14 p-5 rounded-full bg-customOrangeDark flex justify-center items-center">
-                    <img class="min-w-8 " src="{{ asset('assets/icons/dashborad icons2.png') }}" alt="">
+                <div class="flex items-center justify-center rounded-full">
+                    <img class="h-14 w-14" src="{{ asset('assets/icons/dex icon2.png') }}" alt="">
                 </div>
             </div>
             <div
                 class="flex items-center justify-between w-full h-full p-4 bg-orange-100 border-4 shadow-sm border-customOrangeLight rounded-xl">
                 <div>
                     <h2 class="mb-1 text-sm font-medium text-gray-400">Blogs</h2>
-                    <p class=" text-2xl font-bold text-gray-800">5,732</p>
+                    <p class="text-2xl font-bold text-gray-800 ">5,732</p>
                 </div>
-                <div class="w-14 h-14 p-5 flex justify-center items-center rounded-full bg-customOrangeDark">
-                    <img class="min-w-7" src="{{ asset('assets/icons/dashborad icons3.png') }}" alt="">
+                <div class="flex items-center justify-center rounded-full">
+                    <img class="h-14 w-14" src="{{ asset('assets/icons/dex icon3.png') }}" alt="">
                 </div>
             </div>
             <div
                 class="flex items-center justify-between w-full h-full p-4 bg-orange-100 border-4 shadow-sm border-customOrangeLight rounded-xl">
                 <div>
                     <h2 class="mb-1 text-sm font-medium text-gray-400">Diseases</h2>
-                    <p class=" text-2xl font-bold text-gray-800">10</p>
+                    <p class="text-2xl font-bold text-gray-800 ">10</p>
                 </div>
-                <div class="w-14 h-14 p-5 flex justify-center items-center rounded-full bg-customOrangeDark">
-                    <img class="min-w-7" src="{{ asset('assets/icons/dashborad icons4.png') }}" alt="">
+                <div class="flex items-center justify-center rounded-full">
+                    <img class="h-14 w-14" src="{{ asset('assets/icons/dex icon4.png') }}" alt="">
                 </div>
             </div>
         </div>
