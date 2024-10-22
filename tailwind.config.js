7b4c0c7a857b0ebc76b091c9c1c3d49c0406abfd
MuhammadNasir1 <nmian7080@gmail.com>
2024-10-22 14:31:10 +0500

    change breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,22 @@ module.exports = {
                 custom16: "16px", // Add your custom font size here
             },
         },
+        screens: {
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            xl: "1320px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+        },
     },
     plugins: [require("flowbite/plugin")],
 };
