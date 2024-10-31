ca241bdc709d15c05479931a678724d202f7bf21
MuhammadNasir1 <nmian7080@gmail.com>
2024-10-31 16:15:36 +0500

    insert and delete city

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
+    // Post Request Delete Data variable
+
     // submit btn loading spinner show function
     function BtnSpinnerShow() {
         $("#btnSpinner").removeClass("hidden");
         $("#btnText").addClass("hidden");
         $("#submitBtn").attr("disabled", true);
     }
+
     // submit btn loading spinner hide function
     function BtnSpinnerHide() {
         $("#btnSpinner").addClass("hidden");
@@ -12,8 +15,86 @@ $(document).ready(function () {
         $("#submitBtn").attr("disabled", false);
     }
 
+    // Success response sweet alert
+    function SuccessAlert(message) {
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `<span class="font-semibold text-green-600">${message}</span>`,
+            showConfirmButton: false,
+            timer: 2000,
+        });
+    }
+    // Error response sweet alert
+
+    function WarningAlert(message) {
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `<span class="font-semibold text-green-600">${message}</span>`,
+            showConfirmButton: false,
+            timer: 2000,
+        });
+    }
+
+    // Reload dataTable function
+    function reloadDataTable() {
+        // Destroy the existing DataTable instance
+        let table = $("#datatable").DataTable();
+        table.destroy();
+        $("#loading").show();
+
+        // Reload the table body content
+        $("#tableBody").load(" #tableBody > *", function () {
+            // Reinitialize DataTable after loading new data
+            $("#datatable").DataTable();
+            delDataFun();
+            $("#loading").hide();
+        });
+    }
+
+    function delDataFun() {
+        $(".deleteDataBtn").click(function () {
+            let id = $(this).attr("delId");
+            let url = "/deleteCities";
+            let csrfToken = $('meta[name="csrf-token"]').attr("content");
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: {
+                    city_id: id,
+                },
+                headers: {
+                    "X-CSRF-TOKEN": csrfToken,
+                },
+                beforeSend: function () {
+                    $("#loading").show();
+                },
+
+                success: function (response) {
+                    // $("#tableBody").load(" #tableBody > *");
+                    $("#loading").hide();
+                    // Reload the table body content
+                    reloadDataTable();
+                    $(document).trigger("formSubmissionResponse", [
+                        response,
+                        SuccessAlert(response.message),
+                    ]);
+                },
+
+                // error: function (error) {
+                //     $(document).trigger("formSubmissionResponse", [
+                //         response,
+                //         WarningAlert(response.message),
+                //     ]);
+                // },
+            });
+        });
+    }
+    delDataFun();
     // post data ajax request
-    $("#dataForm").submit(function (e) {
+    $("#postDataForm").submit(function (e) {
         e.preventDefault();
         let url = $(this).attr("action");
         let formData = $(this).serialize();
@@ -25,42 +106,34 @@ $(document).ready(function () {
 
             beforeSend: function () {
                 BtnSpinnerShow();
-                $("#dataForm")[0].reset();
+                $("#postDataForm")[0].reset();
             },
 
             success: function (response) {
+                // $("#tableBody").load(" #tableBody > *");
                 BtnSpinnerHide();
 
-                $("#tableBody").load(" #tableBody > *");
-                const Alert = Swal.fire({
-                    position: "center",
-                    icon: "success",
-                    title: `<span class="font-semibold text-green-600">${response.message}</span>`,
-                    showConfirmButton: false,
-                    timer: 2000,
-                });
+                // Reload the table body content
+                reloadDataTable();
                 $(document).trigger("formSubmissionResponse", [
                     response,
-                    Alert,
+                    SuccessAlert(response.message),
                 ]);
+                delDataFun();
             },
 
             error: function (error) {
                 BtnSpinnerHide();
-
-                const Alert = Swal.fire({
-                    position: "center",
-                    icon: "success",
-                    title: `<span class="font-semibold text-green-600">${response.message}</span>`,
-                    showConfirmButton: false,
-                    timer: 2000,
-                });
                 console.log("Error:", error);
                 $(document).trigger("formSubmissionResponse", [
                     response,
-                    Alert,
+                    WarningAlert(response.message),
                 ]);
             },
         });
+    });
+
+    $("#datatable").on("draw", function () {
+        delDataFun(); // Ensures the delete button listener is reattached on draw
     });
 });
