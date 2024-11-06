eb0045bd1fe6f7c2d3a5045ff14cf782cad8d953
MuhammadNasir1 <nmian7080@gmail.com>
2024-11-06 17:36:09 +0500

    show operators and priveleges data

--- resources/views/operators.blade.php
+++ resources/views/operators.blade.php
@@ -8,30 +8,57 @@
             <h1 class="text-3xl font-bold ps-5">Operators</h1>
         </div>
         @php
-            $headers = ['Sr.', 'Image', 'Name', 'Email', 'Phone Number', 'Last Active', 'status', 'Action'];
+            $headers = ['Sr.', 'Image', 'Name', 'Email', 'Phone Number', 'status', 'Action'];
         @endphp
 
         <x-table :headers="$headers">
             <x-slot name="tablebody">
-                <tr>
-                    <td>1</td>
-                    <td><img src="{{ asset('assets/Profile photo (1) 1.png') }}" alt=''></td>
-                    <td>Kurt Weissnat</td>
-                    <td>KurtWeissnat@email.com</td>
-                    <td>1234564</td>
-                    <td>1234564</td>
-                    <td><span class='text-blue-500'>active</span></td>
-                    <td>
-                        <span class='flex justify-center'>
-                           <a href="../priveleges">
-                            <button>
-                                <img src="{{ asset('assets/icons/edit icon.png') }}" alt=''>
-                            </button>
-                           </a>
-                        </span>
-                    </td>
-                </tr>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td><img class="h-16 w-16 rounded-full bg-black object-contain"
+                                src="{{ asset('assets/Profile phoo (1) 1.png') }}" alt='{{ $user->name }}'></td>
+                        <td>{{ $user->name }}</td>t
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->user_phone }}</td>
+                        <td><button data-modal-target="status-modal"
+                                data-modal-toggle="status-modal">{!! $user->user_status == 1
+                                    ? "<span class='text-blue-500'>Active</span>"
+                                    : "<span class='text-red-600'>Un-Active</span>" !!}</button>
+                        </td>
+                        <td>
+                            <span class='flex justify-center'>
+                                <a href="../priveleges/{{ $user->id }}">
+                                    <button>
+                                        <img src="{{ asset('assets/icons/edit icon.png') }}" alt=''>
+                                    </button>
+                                </a>
+                            </span>
+                        </td>
+                    </tr>
+                @endforeach
+
             </x-slot>
         </x-table>
     </div>
+
+    <x-modal id="status-modal">
+        <x-slot name="title">Add Cities</x-slot>
+        <x-slot name="modal_width">max-w-4xl</x-slot>
+        <x-slot name="body">
+            <form id="postDataForm" url="saveCities" method="post">
+                @csrf
+                <input type="hidden" class="text" id="updateId" name="city_id">
+                <div class="grid grid-cols-2 gap-4">
+                    <x-input id="cityName" label="City Name" placeholder="Enter City" name='city_name'
+                        type="text"></x-input>
+                    <x-input id="cityProvince" label="city Province" placeholder=" Enter City Province" name='city_province'
+                        type="text"></x-input>
+                </div>
+                <div class="mt-4">
+                    <x-modal-button :title="'Add City'"></x-modal-button>
+                </div>
+            </form>
+        </x-slot>
+    </x-modal>
 @endsection
