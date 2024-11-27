8fbecb700a76f2d8780e205433add5272c3b9013
MuhammadNasir1 <nmian7080@gmail.com>
2024-11-27 15:07:44 +0500

    Add Peding media

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -107,6 +107,9 @@ class MediaController extends Controller
                     $blog->date = Carbon::parse($blog->created_at)->format('M d, Y');
                     $category = $categories->firstWhere('category_id', $blog->category_id);
                     $blog->category_name = $category ? $category->category_name : null;
+                     
+                    $username = User::where('id', $blog->added_user_id)->value('name');
+                    $blog->added_username = $username;
                 }
                 return view('blogs', ['media' => $media, "categories" => $categories]);
             } elseif ($type == 'diseases') {
@@ -116,6 +119,9 @@ class MediaController extends Controller
                     $diseases->date = Carbon::parse($diseases->created_at)->format('M d, Y');
                     $category = $categories->firstWhere('category_id', $diseases->category_id);
                     $diseases->category_name = $category ? $category->category_name : null;
+                     
+                    $username = User::where('id', $diseases->added_user_id)->value('name');
+                    $diseases->added_username = $username;
                 }
                 return view('diseases', ['media' => $media, "categories" => $categories]);
             } elseif ($type == 'consultancy') {
@@ -125,6 +131,9 @@ class MediaController extends Controller
                     $diseases->date = Carbon::parse($diseases->created_at)->format('M d, Y');
                     $category = $categories->firstWhere('category_id', $diseases->category_id);
                     $diseases->category_name = $category ? $category->category_name : null;
+                     
+                    $username = User::where('id', $diseases->added_user_id)->value('name');
+                    $diseases->added_username = $username;
                 }
                 return view('consultancyvideos', ['media' => $media, "categories" => $categories]);
             } elseif ($type == 'pending') {
@@ -134,6 +143,11 @@ class MediaController extends Controller
                     $diseases->date = Carbon::parse($diseases->created_at)->format('M d, Y');
                     $category = $categories->firstWhere('category_id', $diseases->category_id);
                     $diseases->category_name = $category ? $category->category_name : null;
+
+                    $username = User::where('id', $diseases->added_user_id)->value('name');
+                    $diseases->added_username = $username;
+
+
                 }
                 // return response()->json($media);
                 return view('pending_media', ['media' => $media, "categories" => $categories]);
--- resources/views/pending_media.blade.php
+++ resources/views/pending_media.blade.php
@@ -9,65 +9,80 @@
                 <div>
                     <h1 class="text-3xl font-bold">Pending Media</h1>
                 </div>
-             
+
             </div>
 
         </div>
         <button data-modal-target='view-modal' data-modal-toggle='view-modal'></button>
         <div id="categoryTable" class="transition-opacity duration-500 ">
             @php
-            $headers = ['Sr.', 'Added By' , 'Title', 'Description', 'Category', 'Date', 'Author', 'Action'];
-        @endphp
+                $headers = [
+                    'Sr.',
+                    'Added By',
+                    'Title',
+                    'Description',
+                    'Category',
+                    'Date',
+                    'Author',
+                    'Status',
+                    'Action',
+                ];
+            @endphp
 
             <x-table :headers="$headers">
                 <x-slot name="tablebody">
                     @foreach ($media as $data)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td class="whitespace-nowrap">{{ $data->added_username  }}</td>
-                        {{-- <td>{{ $username =App\Models\User::select('name')->where('id' , $data->added_user_id)->first();  }}</td> --}}
-         
-                        <td class='text-xs xl:text-[15px]'>{{ $data->media_title }}</td>
-                        <td class='text-xs xl:text-[15px] min-w-[280px]'>{{ \Illuminate\Support\Str::limit($data->media_description, 60, '...') }}</td>
-                        <td class='text-sm xl:text-[15px] whitespace-nowrap'>{{ $data->category_name }}</td>
-                        <td class='text-sm xl:text-[15px] whitespace-nowrap'>{{ $data->date }} </td>
-                        <td class='text-sm xl:text-[15px] whitespace-nowrap'>{{ $data->media_author }}</td>
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td class="whitespace-nowrap">{{ $data->added_username }}</td>
+                            {{-- <td>{{ $username =App\Models\User::select('name')->where('id' , $data->added_user_id)->first();  }}</td> --}}
 
-                        <td>
-                            <span class='flex gap-4'>
+                            <td class='text-xs xl:text-[15px]'>{{ $data->media_title }}</td>
+                            <td class='text-xs xl:text-[15px] min-w-[280px]'>
+                                {{ \Illuminate\Support\Str::limit($data->media_description, 60, '...') }}</td>
+                            <td class='text-sm xl:text-[15px] whitespace-nowrap'>{{ $data->category_name }}</td>
+                            <td class='text-sm xl:text-[15px] whitespace-nowrap'>{{ $data->date }} </td>
+                            <td class='text-sm xl:text-[15px] whitespace-nowrap'>{{ $data->media_author }}</td>
+                            <td class='text-sm xl:text-[15px] whitespace-nowrap'><button
+                                    class="text-red-600 font-semibold">Pending</button></td>
 
-                                <button
-                                    mediaTitle="{{ $data->media_title }}" mediaAuthor="{{ $data->media_author }}"
-                                    mediaCategory="{{ $data->category_name }}" mediaCategoryId={{ $data->category_id }}
-                                    mediaDate="{{ $data->date }}" mediaDescription="{{ $data->media_description }}"
-                                    mediaId="{{ $data->media_id }}"
-                                    mediaImage="{{  $data->media_image ??  asset('assets/default-logo-req.png') }}"
-                                    class="viewModalBtn">
-                                    <svg width='37' height='36' viewBox='0 0 37 36' fill='none'
-                                        xmlns='http://www.w3.org/2000/svg'>
-                                        <path fill-rule='evenodd' clip-rule='evenodd'
-                                            d='M28.0642 18.5C28.0642 18.126 27.8621 17.8812 27.4579 17.3896C25.9788 15.5938 22.7163 12.25 18.9288 12.25C15.1413 12.25 11.8788 15.5938 10.3996 17.3896C9.99542 17.8812 9.79333 18.126 9.79333 18.5C9.79333 18.874 9.99542 19.1187 10.3996 19.6104C11.8788 21.4062 15.1413 24.75 18.9288 24.75C22.7163 24.75 25.9788 21.4062 27.4579 19.6104C27.8621 19.1187 28.0642 18.874 28.0642 18.5ZM18.9288 21.625C19.7576 21.625 20.5524 21.2958 21.1385 20.7097C21.7245 20.1237 22.0538 19.3288 22.0538 18.5C22.0538 17.6712 21.7245 16.8763 21.1385 16.2903C20.5524 15.7042 19.7576 15.375 18.9288 15.375C18.0999 15.375 17.3051 15.7042 16.719 16.2903C16.133 16.8763 15.8038 17.6712 15.8038 18.5C15.8038 19.3288 16.133 20.1237 16.719 20.7097C17.3051 21.2958 18.0999 21.625 18.9288 21.625Z'
-                                            fill='url(#paint0_linear_872_5570)' />
-                                        <circle opacity='0.1' cx='18.4287' cy='18' r='18'
-                                            fill='url(#paint1_linear_872_5570)' />
-                                        <defs>
-                                            <linearGradient id='paint0_linear_872_5570' x1='18.9288' y1='12.25'
-                                                x2='18.9288' y2='24.75' gradientUnits='userSpaceOnUse'>
-                                                <stop stop-color='#FCB376' />
-                                                <stop offset='1' stop-color='#FE8A29' />
-                                            </linearGradient>
-                                            <linearGradient id='paint1_linear_872_5570' x1='18.4287' y1='0'
-                                                x2='18.4287' y2='36' gradientUnits='userSpaceOnUse'>
-                                                <stop stop-color='#FCB376' />
-                                                <stop offset='1' stop-color='#FE8A29' />F
-                                            </linearGradient>
-                                        </defs>
-                                    </svg>
-                                </button>
-                            </span>
-                        </td>
-                    </tr>
-                @endforeach
+                            <td>
+                                <span class='flex gap-4'>
+
+                                    <button mediaTitle="{{ $data->media_title }}" mediaAuthor="{{ $data->media_author }}"
+                                        mediaCategory="{{ $data->category_name }}" mediaCategoryId={{ $data->category_id }}
+                                        mediaDate="{{ $data->date }}" mediaDescription="{{ $data->media_description }}"
+                                        mediaId="{{ $data->media_id }}"
+                                        mediaImage="{{ $data->media_image ?? asset('assets/default-logo-req.png') }}"
+                                        class="viewModalBtn">
+                                        <svg width='37' height='36' viewBox='0 0 37 36' fill='none'
+                                            xmlns='http://www.w3.org/2000/svg'>
+                                            <path fill-rule='evenodd' clip-rule='evenodd'
+                                                d='M28.0642 18.5C28.0642 18.126 27.8621 17.8812 27.4579 17.3896C25.9788 15.5938 22.7163 12.25 18.9288 12.25C15.1413 12.25 11.8788 15.5938 10.3996 17.3896C9.99542 17.8812 9.79333 18.126 9.79333 18.5C9.79333 18.874 9.99542 19.1187 10.3996 19.6104C11.8788 21.4062 15.1413 24.75 18.9288 24.75C22.7163 24.75 25.9788 21.4062 27.4579 19.6104C27.8621 19.1187 28.0642 18.874 28.0642 18.5ZM18.9288 21.625C19.7576 21.625 20.5524 21.2958 21.1385 20.7097C21.7245 20.1237 22.0538 19.3288 22.0538 18.5C22.0538 17.6712 21.7245 16.8763 21.1385 16.2903C20.5524 15.7042 19.7576 15.375 18.9288 15.375C18.0999 15.375 17.3051 15.7042 16.719 16.2903C16.133 16.8763 15.8038 17.6712 15.8038 18.5C15.8038 19.3288 16.133 20.1237 16.719 20.7097C17.3051 21.2958 18.0999 21.625 18.9288 21.625Z'
+                                                fill='url(#paint0_linear_872_5570)' />
+                                            <circle opacity='0.1' cx='18.4287' cy='18' r='18'
+                                                fill='url(#paint1_linear_872_5570)' />
+                                            <defs>
+                                                <linearGradient id='paint0_linear_872_5570' x1='18.9288' y1='12.25'
+                                                    x2='18.9288' y2='24.75' gradientUnits='userSpaceOnUse'>
+                                                    <stop stop-color='#FCB376' />
+                                                    <stop offset='1' stop-color='#FE8A29' />
+                                                </linearGradient>
+                                                <linearGradient id='paint1_linear_872_5570' x1='18.4287' y1='0'
+                                                    x2='18.4287' y2='36' gradientUnits='userSpaceOnUse'>
+                                                    <stop stop-color='#FCB376' />
+                                                    <stop offset='1' stop-color='#FE8A29' />F
+                                                </linearGradient>
+                                            </defs>
+                                        </svg>
+                                    </button>
+                                    <button
+                                        class="text-sm bg-green-800 text-white py-3 px-4 font-semibold rounded-xl approveMediaBtn whitespace-nowrap"
+                                        Id="{{ $data->media_id }}">Approve Media</button>
+                                </span>
+                            </td>
+                        </tr>
+                    @endforeach
                 </x-slot>
             </x-table>
         </div>
@@ -79,8 +94,9 @@
                     <div class="flex">
                         <!-- Image Placeholder -->
 
-                        <img class="w-40 h-40 bg-black object-contain" id="dImage"
-                            src="{{ asset('assets/Surface 3 png.png') }}" alt="">
+                        <div id="mediaOutput">
+
+                        </div>
 
 
                         <!-- Text Details -->
@@ -110,70 +126,108 @@
                 </div>
             </x-slot>
         </x-modal>
-     
+    @endsection
+    @section('js')
+        <script>
+            let table = $("#datatable").DataTable();
+
+            function viewData() {
+                $(".approveMediaBtn").click(function() {
+                    let csrfToken = $('meta[name="csrf-token"]').attr("content");
+                    let id = $(this).attr("Id");
+                    let dynamicKey = "media_id";
+                    let Data = {};
+                    Data[dynamicKey] = id;
+                    // Show SweetAlert  confirmation dialog
+                    Swal.fire({
+                        title: "Change status to approved",
+                        text: "You won't be able to revert this!",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "green",
+                        cancelButtonColor: "gray",
+                        confirmButtonText: "Yes, Change it!",
+
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            // If confirmed, proceed with AJAX request to delete
+                            $.ajax({
+                                type: "POST",
+                                url: "../approveMedia",
+                                data: Data,
+                                headers: {
+                                    "X-CSRF-TOKEN": csrfToken,
+                                },
+                                beforeSend: function() {},
+                                success: function(response) {
+                                    table.destroy();
+
+                                    $("#tableBody").load(" #tableBody > *", function() {
+                                        updateDatafun();
+                                        $("#datatable").DataTable();
+                                    });
+
+                                    const alert = Swal.fire({
+                                        title: "Status Changed!",
+                                        text: response.message,
+                                        icon: "success",
+                                    });
+
+                                    $(document).trigger("formSubmissionResponse", [
+                                        response,
+                                        alert,
+                                    ]);
+                                },
+                                error: function(xhr) {
+                                    $("#loading").hide();
+                                    Swal.fire({
+                                        title: "Error!",
+                                        text: "There was an error in change  status.",
+                                        icon: "error",
+                                    });
+                                },
+                            });
+                        }
+                    });
+                });
 
 
-@endsection
-@section('js')
-   <script>
-        function viewData() {
+                $('.viewModalBtn').click(function() {
+                    $('#mediaOutput').html('');
+                    $('#view-modal').addClass('flex').removeClass('hidden');
+                    $('#dTitle').text($(this).attr('mediaTitle'));
+                    $('#dAuthor').text($(this).attr('mediaAuthor'));
+                    $('#dCategory').text($(this).attr('mediaCategory'));
+                    $('#dDate').text($(this).attr('mediaDate'));
+                    $('#dDescription').text($(this).attr('mediaDescription'));
+                    let mediaUrl = $(this).attr('mediaImage'); 
+                    let mediaContent;
+                    if (mediaUrl && /\.(jpg|jpeg|png|gif|svg)$/i.test(mediaUrl)) {
+                         mediaContent = `<img class="w-40 h-40 bg-black object-contain" id="dImage"  src="${mediaUrl}" alt="Media Image">`;
+                    } else if (mediaUrl && /\.(mp4|webm|ogg)$/i.test(mediaUrl)) { // Check if it's a video
+                        mediaContent = `<video class="w-52 h-48 bg-black object-contain" controls id="dImage" src="${mediaUrl}"></video>`;
+                    } else {
+                        mediaContent = `<img class="w-40 h-40 bg-black object-contain" id="dImage" src="{{asset('assets/default-logo-req.png')}}" alt="Media Image">`;
+                    }
 
-            $('.viewModalBtn').click(function() {
-                $('#view-modal').addClass('flex').removeClass('hidden');
-                $('#dTitle').text($(this).attr('mediaTitle'));
-                $('#dAuthor').text($(this).attr('mediaAuthor'));
-                $('#dCategory').text($(this).attr('mediaCategory'));
-                $('#dDate').text($(this).attr('mediaDate'));
-                $('#dDescription').text($(this).attr('mediaDescription'));
-                $('#dImage').attr('src', $(this).attr('mediaImage'));
+                    $('#mediaOutput').append(mediaContent)
+                });
 
-            });
-
-        }
-        viewData()
-
-        function updateDatafun() {
+            }
             viewData()
-            $('.updateDataBtn').click(function() {
-                $('#blog-modal').removeClass("hidden").addClass('flex');
 
-                let mediaDetails = $(this).siblings('.viewModalBtn');;
-                $('#updateId').val(mediaDetails.attr('mediaId'));
-                $('#mediaTitle').val(mediaDetails.attr('mediaTitle'));
-                $('#mediaTitle').val(mediaDetails.attr('mediaTitle'));
-                $('#mediaAuthor').val(mediaDetails.attr('mediaAuthor'));
-                $('#categoryId').val(mediaDetails.attr('mediaCategoryId')).trigger('change');
-                $('#mediaDescription').val(mediaDetails.attr('mediaDescription'));
-                let fileImg = $('#blog-modal .file-preview');
-                fileImg.removeClass('hidden').attr('src', mediaDetails.attr('mediaImage'));
+            function updateDatafun() {
+                viewData()
+            }
+            updateDatafun();
 
+            // Listen for the custom form submission response event
+            $(document).on("formSubmissionResponse", function(event, response, Alert, SuccessAlert, WarningAlert) {
+                // console.log(response);
+                if (response.success) {
 
-                $('#blog-modal #modalTitle').text("Update Blog");
-                $('#blog-modal #btnText').text("Update");
-
+                    $('.modalCloseBtn').click();
+                } else {}
             });
-        }
-        updateDatafun();
-        $('#addModalBtn').click(function() {
-            $('#postDat aForm')[0].reset();
-            $('#categoryId').trigger('change');
-            $('#updateId').val('');
-            $('#blog-modal #modalTitle').text("Add Blog");
-            $('#blog-modal #btnText').text("Add");
-            let fileImg = $('#blog-modal .file-preview');
-            fileImg.addClass('hidden');
-
-        })
-
-
-        // Listen for the custom form submission response event
-        $(document).on("formSubmissionResponse", function(event, response, Alert, SuccessAlert, WarningAlert) {
-            // console.log(response);
-            if (response.success) {
-
-                $('.modalCloseBtn').click();
-            } else {}
-        });
-    </script>
-@endsection
-
+        </script>
+    @endsection
