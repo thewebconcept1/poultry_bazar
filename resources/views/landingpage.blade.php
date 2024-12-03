b803c181e34478ae8431beb6b9af5a0c063426ba
MuhammadNasir1 <nmian7080@gmail.com>
2024-12-03 16:10:29 +0500

    Add module button in sidebar

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', '') - Poultry Bazar</title>
+    <title>@yield('title', 'Home') - Poultry Bazar</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
