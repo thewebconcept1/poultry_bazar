ef9b5707e66365572ff4ae1786a5eee324a77f9a
sufyanamir <sufi.canola@gmail.com>
2024-12-05 11:56:25 +0500

    Made new migrations and model of company, products and product variations.

---
+++ app/Models/ProductVariations.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariations extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product_variations';
+
+    protected $primaryKey = 'product_variation_id';
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'product_name',
+        'variation_name',
+        'variation_sale_rate',
+        'variation_consumed',
+        'variation_wastage',
+        'variation_status',
+    ];
+
+    public $timestamps = true;
+
+}
---
+++ database/migrations/2024_12_05_063724_product_variations.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variations', function (Blueprint $table) {
+            $table->id('product_variation_id');
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->string('product_name');
+            $table->string('variation_name');
+            $table->float('variation_sale_rate')->default(0);
+            $table->float('variation_consumed')->default(0);
+            $table->float('variation_wastage')->default(0);
+            $table->integer('variation_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variations');
+    }
+};
