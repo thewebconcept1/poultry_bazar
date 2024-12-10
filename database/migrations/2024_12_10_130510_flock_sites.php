07e19899e983e529ca2ff480e315b2aba39826d6
sufyanamir <sufi.canola@gmail.com>
2024-12-10 18:52:20 +0500

    made migration

---
+++ database/migrations/2024_12_10_130510_flock_sites.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flock_sites', function(Blueprint $table) {
+            $table->id('site_id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('url');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
